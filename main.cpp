9cea89d74f685f167cc90f22349636aadb8286c2
Adam Kusnierz <adam.kusnierz@gmail.com>
2021-10-08 13:14:56 +0200

    First entry

--- main.cpp
+++ main.cpp
@@ -1,21 +1,94 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <cstdbool>
-
+#include <algorithm>
 using namespace std;
 
 int main() {
+	//global variables
+	bool mainProgram = true;
+	string playerName;
 
-	string input[] = {"Adventure", "Battle"};
+	//local variables
+	bool playerNameLoop = true;
+	char tryAgain;
 
-	cout << "Welcome to my adventure game\n";
-	cout << "Choose\n\tAdventure\tBattle\n";
+	//premenu enter character name
+	while (playerNameLoop)
+    		{
+	   			cout << "Enter your Character name: ";
+	   			cin >> playerName;
 
-	if()
+	   			cout << "You inputted " << playerName << endl;
+
+	   			cout << "Try Again?(Y/N): ";
+	   			cin >> tryAgain;
+
+				tryAgain = (toupper(tryAgain));
+	   			if (tryAgain == 'Y')
+		  		playerNameLoop = true;
+
+	   			else if (tryAgain == 'N')
+		  		playerNameLoop = false;
+
+	   			else
+	   			{	
+		 		cout << "Your input " << tryAgain << " is not valid.\n";
+		  		cout << "This program will assume that you want to continue.\n";
+		  		playerNameLoop = true;
+	   			}
+    		}
+
+	while (mainProgram)
 	{
+		//local variables
+		string userInput;
+		
+		//Welcome and present options
+		cout << "Dibablo 2\n";
+		    
+
+		cout << "Choose\n\tAdventure\tArena\t Exit\n";
+
+		//user intput and transform to uppercase 		maby redo to a function!!
+		cout << playerName << " input: ";
+		cin >> userInput;
+		transform(userInput.begin(), userInput.end(), userInput.begin(), ::toupper);
+
+		if (userInput == "ADVENTURE")
+		{
+				cout << "Welcome to Blood Smoor\n";
+		}
+		else if (userInput == "BATTLE");
+		{
+			//local variables
+			bool battleProgram = true;
+
+			while (battleProgram)
+			{
+				//local variables
+				string userInput;
+
+				cout << "Welcome to Arena\nChoose monster to fight:\n";
+				cout << "Skeleton\t\n";
+				
+				//user intput and transform to uppercase 		maby redo to a function!!
+				cout << playerName << " input: ";
+				cin >> userInput;
+				transform(userInput.begin(), userInput.end(), userInput.begin(), ::toupper);
+				
+				if (userInput == "Skelleton");
+				{
+					//local variables
+					bool skeleton = true;
+					while (skeleton)
+					{
+						"A skeleton attacks!";
+					}
+				}
+			}
+		}
 
 	}
-	
 	return 0;
 }
